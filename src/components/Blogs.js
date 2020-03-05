01540ace2c6d3ef867dc6295b8fba33f60614589
Keith <keith.ong5000@gmail.com>
2020-03-05 10:36:13 -0800

    Add react-bootstrap to the project, notes now displayed in bootstrap table

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { Table } from 'react-bootstrap'
 
 const Blogs = (({ blogs }) => {
 
     return (
-        <div>
-            {blogs.map(blog => {
-                return (
-                    <div>
-                        <Link to={`/api/blogs/${blog._id}`}>{blog.title}</Link>
-                    </div>
-                )
-            }
-
-            )}
-        </div>
+        <Table striped>
+            <tbody>
+                {blogs.map(blog =>
+                    <tr key={blog._id}>
+                        <td>
+                            <Link to={`/api/blogs/${blog._id}`}>{blog.title}</Link>
+                        </td>
+                    </tr>
+                )}
+            </tbody>
+        </Table>
     )
 })
 
