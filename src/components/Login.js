8613f76eb976cfdb7244ff75aa54e7d0256bca77
Keith <keith.ong5000@gmail.com>
2020-03-05 10:44:36 -0800

    Used bootstrap form for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { userReducer, login} from '../reducers/userReducer'
+import { login } from '../reducers/userReducer'
 import { connect } from 'react-redux'
+import { Form, Button } from 'react-bootstrap'
 
 const Login = (props) => {
     const handleLogin = (event) => {
@@ -9,26 +10,48 @@ const Login = (props) => {
         const passwordLogin = event.target.password.value
         props.login(usernameLogin, passwordLogin)
     }
-
-    return(
-        <form onSubmit={handleLogin}>
-            <div>
-                Username
-                <input
-                type="text"
-                name="username"
-                ></input>
-            </div>
-            <div>
-                Password
-                <input
-                type="password"
-                name="password"
-                ></input>
-            </div>
-            <button type ="submit">login</button>
-        </form>
+    return (
+        <div>
+            <h2>login</h2>
+            <Form onSubmit={handleLogin}>
+                <Form.Group>
+                    <Form.Label>username</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="username"
+                    />
+                    <Form.Label>password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                    />
+                    <Button variant="primary" type ="submit">
+                        login
+                    </Button>
+                </Form.Group>
+            </Form>
+        </div>
     )
+
+    // return(
+    //     <form onSubmit={handleLogin}>
+    //         <div>
+    //             Username
+    //             <input
+    //             type="text"
+    //             name="username"
+    //             ></input>
+    //         </div>
+    //         <div>
+    //             Password
+    //             <input
+    //             type="password"
+    //             name="password"
+    //             ></input>
+    //         </div>
+    //         <button type ="submit">login</button>
+    //     </form>
+    // )
 }
 
 const mapDispatchToProps = {
